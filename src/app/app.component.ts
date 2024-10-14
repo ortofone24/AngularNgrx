5597f037b2189428be1b17eb50b472e479d04e86
ortofone24 <ortofone24@gmail.com>
2024-10-14 18:59:14 +0200

    add post list and header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter/counter.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CounterComponent],
+  imports: [RouterOutlet, RouterLink, CounterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
