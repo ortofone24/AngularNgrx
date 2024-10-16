56fa45c412f76029dfeaf6af8eb60dac4dbc7ba3
ortofone24 <ortofone24@gmail.com>
2024-10-16 13:34:39 +0200

    add validations

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.css'
 })
@@ -16,7 +17,45 @@ export class AddPostComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.postForm = new FormGroup({
+      title: new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]),
+      description: new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.minLength(10)
+        ])
+    })
+  }
 
+  showDescriptionErrors() {
+    const descriptionForm = this.postForm.get('description')
+
+    if (descriptionForm?.touched && !descriptionForm.valid) {
+
+      if (descriptionForm?.errors?.['required']) {
+        return 'Description is required';
+      }
+      if (descriptionForm?.errors?.['minlength']) {
+        return 'Description  should be minimum 10 characters';
+      }
+    }
+    return null;
+  }
+
+  onAddPost() {
+    console.log(this.postForm)
+    if (!this.postForm.valid) {
+      return;
+    }
+
+    console.log(this.postForm.value);
   }
 
 }
+
