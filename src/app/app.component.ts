6d7095f06f74f50b69ff23b7b01193c975fb83e7
ortofone24 <ortofone24@gmail.com>
2024-10-30 09:33:56 +0100

    add spinner loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter/counter.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CounterComponent, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, CounterComponent, HeaderComponent, LoadingSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
